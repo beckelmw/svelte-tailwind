fa5be2b7f390f78924ac1aca5975d5f1e19da4cd
BBeckelman <bbeckelman@grubhub.com>
2020-06-24 07:04:28 -0700

    Added serverless for deployment. Improved generation

--- generate.js
+++ generate.js
@@ -1,38 +1,50 @@
-const { writeFile } = require("fs");
-const { promisify } = require("util");
-const writeFileAsync = promisify(writeFile);
+const { writeFile, mkdir, copy } = require("fs-extra");
+const { dirname } = require("path");
+
 const app = require("./public/build/app.js");
 
 (async () => {
   const pages = [
     { url: "/", file: "index.html" },
-    { url: "/page/1", file: "page/1.html" },
-    { url: "/page/2", file: "page/2.html" },
-    { url: "/page/3", file: "page/3.html" },
+    { url: "/page/1", file: "page/1/index.html" },
+    { url: "/page/2", file: "page/2/index.html" },
+    { url: "/page/3", file: "page/3/index.html" },
   ];
 
   for (const i of pages) {
-    const { html } = app.render({ url: i.url });
-
+    const { html, head } = app.render({ url: i.url });
     const output = `
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-    
-        <title>Tailwind</title>
-    
-        <link rel="icon" type="image/png" href="/favicon.png" />
-        <link rel="stylesheet" href="/build/site.css" />
-        <link rel="stylesheet" href="/build/bundle.css" />
-      </head>
-    
-      <body>
-        ${html}
-      </body>
-    </html>
-    `;
-    await writeFileAsync(`./generated/${i.file}`, output);
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+
+    ${head}
+
+    <link rel="icon" type="image/png" href="/favicon.png" />
+    <link rel="stylesheet" href="/site.css" />
+  </head>
+
+  <body>
+    ${html}
+  </body>
+</html>
+`;
+
+    const outputPath = `./generated/${i.file}`;
+
+    try {
+      await mkdir(dirname(outputPath), { recursive: true });
+    } catch (err) {}
+
+    await writeFile(outputPath, output);
+    console.log(`Created ${outputPath}`);
   }
+
+  await copy("./public/img", "./generated/img");
+  await copy("./public/build/bundle.css", "./generated/site.css");
+  await copy("./public/favicon.png", "./generated/favicon.png");
+
+  console.log("Copied assets");
 })();
